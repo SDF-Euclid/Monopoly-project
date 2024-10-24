179cfba7dbd91a85a7eff26c7b1549682d18d82b
SDF-Euclid <elij4399@gmail.com>
2024-10-23 22:49:59 -0500

    card.h

---
+++ card.h
@@ -0,0 +1,43 @@
+//Preprocessor directives
+#ifndef card_h
+#define card_h
+
+const char *ChanceCards[] = {
+	"Advance to Boardwalk",
+	"Advance to Go (Collect $200)",
+	"Advance to Illinois Avenue. If you pass Go, collect $200",
+	"Advance to St. Charals Place. If you pass Go, collect $200",
+	"Advance to the nearest Railroad. If unowned, you may but it from the Bank. If owned, pay the owner twice the rental they are entitled to", //included twice on purpose
+	"Advance to the nearest Railroad. If unowned, you may but it from the Bank. If owned, pay the owner twice the rental they are entitled to",
+	"Advance to the nearest Utility. If unowned, you may but it from the Bank. If owned, roll the dice and pay the owner 10 times the value of the dice",
+	"Bank pays you a dividend of $50",
+	"Get Out of Jail Free",
+	"Go Back 3 Spaces",	
+	"Go to Jail. Go directly to Jail, do not pass Go, do not collect $200",
+	"Make general repairs on all your property. For each house pay $25. For each hotel pay $100",
+	"Speeding fine $15",
+	"Take a trip to Reading Railroad. If you pass Go, collect $200",
+	"You have been elected Chairman of the Board. Pay each player $50",	
+	"Your building loan matures. Collect $150",		
+};
+
+const char *CommunityChestCards[] = {
+	"Advance to Go (Collect $200)",
+	"Bank error in your favor. Collect $200",	
+	"Doctor's fee. Pay $50",	
+	"From sale of stock you get $50",	
+	"Get Out of Jail Free",	
+	"Go to Jail. Go directly to jail, do not pass Go, do not collect $200",
+	"Holiday fund matures. Recive $100",	
+	"Income tax refund. Collect $20",	
+	"It is your birthday. Collect $10 from each player",	
+	"Life insurance matures. Collect $100",		
+	"Pay hospital fees of $100",
+	"Pay school fees of $50",	
+	"Receive $25 consultancy fee",	
+	"You are assessed for street repair. $40 per house. $115 per hotel",	
+	"You have won second prize in the beauty contest. Collect $10",		
+	"You inherit $100",	
+};
+
+#endif
